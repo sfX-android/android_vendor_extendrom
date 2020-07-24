8ecbb57c35bec6320024cd011e0d62ce4682b5cb
steadfasterX <steadfasterX@gmail.com>
2020-07-24 13:19:08 +0200

    extra: fix ignored by build
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- get_packages.sh
+++ get_packages.sh
@@ -124,6 +124,20 @@ DEVICE="true"
 write_headers "" WITH_FDROID
 DEVICE=
 write_makefiles "$MY_DIR/repo/fdroid.txt"
+cat >> $ANDROIDMK <<EOEXTR
+ifeq (\$(FDROID_EXTRA_REPOS),true)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := additional_repos.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := \$(TARGET_OUT_ETC)/org.fdroid.fdroid
+LOCAL_SRC_FILES := extra/\$(LOCAL_MODULE)
+include \$(BUILD_PREBUILT)
+
+endif
+
+EOEXTR
 write_footers
 
 exit 0
