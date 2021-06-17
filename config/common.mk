c5f3d6b3fbad9c52eb4aaa27d369fb9e45d8e46e
steadfasterX <steadfasterX@gmail.com>
2021-06-17 17:11:10 +0200

    sh|mk: introduce EOS_GESTURES support
    when EOS_GESTURES is set to true gestures will be enabled in
    2 steps (automatically):
    
    1) during vendor/extendrom/get_prebuilts.sh run
       * the source for TrebuchetQuickStep will be adapted so it won't get
          overwritten by BlissLauncher
    
    2) during build/make
        * an overlay will be activated overwriting the default /e/ one enabling
          the TrebuchetQuickStep recents switch
         (enables short up&release and up&hold for recent activities)
    
    pre-requirements / how to use:
    
    * you need to add the Trebuchet to your local manifest so it get synced
       to packages/apps/Trebuchet
    * vendor/extendrom/get_prebuilts.sh must be run at least once after
       the Trebuchet app has been synced
    * add "export EOS_GESTURES=true" to your device/<vendor>/<model>/vendorsetup.sh
    * build as usual

--- config/common.mk
+++ config/common.mk
@@ -2,3 +2,12 @@
 ifdef EXTENDROM_PACKAGES
 PRODUCT_PACKAGES += $(EXTENDROM_PACKAGES)
 endif
+
+# enable gesture support in /e/ OS
+ifeq ($(EOS_GESTURES),true)
+# set specific overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-gestures
+# build TrebuchetQuickStep (get_prebuilts.sh must be executed once at least)
+PRODUCT_PACKAGES += eOSTrebuchetQuickStep
+endif
