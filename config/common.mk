68921f0b68b4ec21695bed9acb5bbed1eee7d84e
2lucem2 <144850589+2lucem2@users.noreply.github.com>
2023-11-04 16:54:41 +0100

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -16,7 +16,7 @@ endif
 # Custom packages
 
 ifdef EXTENDROM_PACKAGES
-PRODUCT_PACKAGES += $(shell echo $$EXTENDROM_PACKAGES)
+PRODUCT_PACKAGES += $(shell echo $$EXTENDROM_PACKAGES | sed "s/Magisk//g")
 endif
 
 ##################################################################
