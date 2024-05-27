782c17ca9e263f8d5bbe3748c1d137f920fabe92
steadfasterX <steadfasterX@users.noreply.github.com>
2024-05-27 17:32:52 +0200

    tools: adapted copyright

--- tools/extract_utils.sh
+++ tools/extract_utils.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #################################################################################
-# Copyright (C) 2021-2023 steadfasterX <steadfasterX -AT- gmail #DOT# com>
+# Copyright (C) 2021-2024 steadfasterX <steadfasterX -AT- gmail #DOT# com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
